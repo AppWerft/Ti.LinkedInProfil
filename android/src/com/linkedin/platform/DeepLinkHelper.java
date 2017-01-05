01caa21671821b6e667bb690e1b83ac80cd3c0de
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2017-01-05 10:46:24 +0100

    annotation jar added

--- android/src/com/linkedin/platform/DeepLinkHelper.java
+++ android/src/com/linkedin/platform/DeepLinkHelper.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
-//import android.support.annotation.NonNull;
+import android.support.annotation.NonNull;
 import android.util.Log;
 
 import com.linkedin.platform.errors.LIAppErrorCode;
